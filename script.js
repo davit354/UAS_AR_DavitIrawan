5800ab1313090253296355bd6706cc4296dc43f6
davit354 <167663785+davit354@users.noreply.github.com>
2024-07-01 22:44:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Menampilkan pesan selamat datang di halaman start
+if (window.location.pathname.includes('start.html')) {
+    alert('Selamat datang di AR Desain Interior: Rumah Impian');
+}
+
+// Menampilkan pesan panduan di halaman panduan
+if (window.location.pathname.includes('guide.html')) {
+    alert('Berikut adalah langkah-langkah menggunakan aplikasi ini');
+}
+
+// Menampilkan pesan tentang di halaman tentang
+if (window.location.pathname.includes('about.html')) {
+    alert('Aplikasi ini dibuat untuk membantu Anda dalam merancang interior rumah dengan teknologi Augmented Reality (AR)');
+}
